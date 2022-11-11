3d1e6f671b5030fe838193b4098b34db316a8ebf
KonichiwaBot <106711702+apinfauzi1@users.noreply.github.com>
2022-11-11 14:48:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,30 +3,30 @@
 // JANGAN DIHAPUS KALAU MAU DITAMBAHIN
 
 // NUMBER
-global.owner = ['62895327934887'] 
+global.owner = ['6281977815539'] 
 global.mods = [] 
 global.prems = [] 
-global.kontak = ['62895327934887']
+global.kontak = ['6281977815539']
 
 //  INFO BOT 
-global.naown = 'KRIZYN OFC'
-global.noown = '62895327934887'
-global.namebot = 'FORYN FOUR'
-global.nobot = '6289502319715'
+global.naown = 'PINZ OFC'
+global.noown = '6281977815539'
+global.namebot = '𝗞𝗼𝗻𝗶𝗰𝗵𝗶𝘄𝗮𝗕𝗼𝘁𝘇'
+global.nobot = '6285746010806'
 
 //  URL SOSMED
-global.instagram = 'https://instagram.com/itskri99'
+global.instagram = 'https://instagram.com/xzy.pinz'
 global.github = 'https://github.com/krizynofc'
 global.gc = 'https://chat.whatsapp.com/HVyRaNWp18NKMAIHv8NjLP'
 global.web = 'https://lynk.id/kri.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.saweria = 'https://saweria.co/Kricom'
 
 // PAYMANT 
-global.dana = '081360482998'
-global.pulsa = '081360482998'
-global.gopay = '081360482998'
-global.shopay = '081360482998'
-global.ovo = '081360482998'
+global.dana = '081977815539'
+global.pulsa = '081977815539'
+global.gopay = '081977815539'
+global.shopay = '081977815539'
+global.ovo = '081977815539'
 
 // PROFILE ALL 
 global.ppreg = 'https://telegra.ph/file/445721f7cdab701543840.jpg'
@@ -54,10 +54,10 @@ global.fsizedoc = '99999999999999' // default 10TB
 global.fpagedoc = '999'
 
 // WATERMARK  
-global.wm = 'ᬊᬁ 𝐅𝐎𝐑𝐘𝐍 𝐅𝐎𝐔𝐑'
+global.wm = 'ᬊᬁ 𝗞𝗼𝗻𝗶𝗰𝗵𝗶𝘄𝗮𝗕𝗼𝘁𝘇'
 global.watermark = wm
-global.wm2 = '⫹⫺ 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 𝙆𝙧𝙞𝙯𝙮𝙣𝙊𝙛𝙘'
-global.wm3 = '⫹⫺ 𝐅𝐎𝐑𝐘𝐍 𝐅𝐎𝐔𝐑 | BOT WHATSAPP MD'
+global.wm2 = '⫹⫺ 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 𝗣𝗶𝗻𝘇𝗢𝗳𝗰'
+global.wm3 = '⫹⫺ 𝗞𝗼𝗻𝗶𝗰𝗵𝗶𝘄𝗮𝗕𝗼𝘁𝘇 | BOT WHATSAPP MD'
 global.tb = 'WHATSAPP BOT MULTI DEVICE'
 
 //  DEFAULT MENU  
@@ -104,8 +104,8 @@ global.salah = 'Salah ❌\n'
 
 // WATERMARK STICKER 
 global.stiker_wait = '⏳ Stiker sedang Proses'
-global.packname = 'FORYN FOUR'
-global.author = 'By krizynOfc'
+global.packname = '𝗞𝗼𝗻𝗶𝗰𝗵𝗶𝘄𝗮𝗕𝗼𝘁𝘇'
+global.author = 'By PinzzzOfc'
 
 //  APIKEY ANDA 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
